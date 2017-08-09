6af1290a0885a8db235694d34ccc3f0cdd656d76
JV Talan <jvtalan1@up.edu.ph>
2017-08-09 14:32:32 +0800

    Add landing page

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    title 'Title'
+    description 'Description'
+    image_url 'http://www.whatever.com/shirt.jpg'
+    price 29
+  end
+end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -44,7 +44,7 @@ describe 'product page navigation', type: :feature do
             expect(page).to have_content "Title can't be blank"
             expect(page).to have_content "Description can't blank"
             expect(page).to have_content "Image url can't be blank"
-            epxect(page).to have_content "Price is not a number"
+            expect(page).to have_content "Price is not a number"
           end
         end
 
@@ -59,7 +59,7 @@ describe 'product page navigation', type: :feature do
           it 'will not return an error for other fields' do
             expect(page).not_to have_content "Description can't blank"
             expect(page).not_to have_content "Image url can't be blank"
-            epxect(page).not_to have_content "Price is not a number"
+            expect(page).not_to have_content "Price is not a number"
           end
         end
 
@@ -74,7 +74,7 @@ describe 'product page navigation', type: :feature do
           it 'will not return an error for other fields' do
             expect(page).not_to have_content "Title can't blank"
             expect(page).not_to have_content "Image url can't be blank"
-            epxect(page).not_to have_content "Price is not a number"
+            expect(page).not_to have_content "Price is not a number"
           end
         end
 
@@ -89,7 +89,7 @@ describe 'product page navigation', type: :feature do
           it 'will not return an error for other fields' do
             expect(page).not_to have_content "Title can't blank"
             expect(page).not_to have_content "Description can't be blank"
-            epxect(page).not_to have_content "Price is not a number"
+            expect(page).not_to have_content "Price is not a number"
           end
         end
 
@@ -104,7 +104,50 @@ describe 'product page navigation', type: :feature do
           it 'will not return an error for other fields' do
             expect(page).not_to have_content "Title can't blank"
             expect(page).not_to have_content "Description can't be blank"
-            epxect(page).not_to have_content "Image url can't be blank"
+            expect(page).not_to have_content "Image url can't be blank"
+          end
+        end
+      end
+
+      context 'when title already exists' do
+        let!(:product) { FactoryGirl.create(:product, title: 'Shirt') }
+
+        it 'returns an error for Title' do
+          visit '/products'
+          click_link 'New Product'
+          fill_in 'Title', with: 'Shirt'
+          fill_in 'Description', with: 'This is a good shirt'
+          fill_in 'Image url', with: 'http://test.com/shirt.jpg'
+          fill_in 'Price', with: '200'
+          click_button 'Create Product'
+
+          expect(page).to have_content 'Title has already been taken'
+        end
+      end
+
+      context 'when Price is invalid' do
+        before do
+          visit '/products'
+          click_link 'New Product'
+          fill_in 'Title', with: 'Shirt'
+          fill_in 'Description', with: 'This is a good shirt'
+          fill_in 'Image url', with: 'http://test.com/shirt.jpg'
+          fill_in 'Price', with: '200'
+        end
+
+        context 'and it is not a number' do
+          it 'returns an error for Price' do
+            fill_in 'Price', with: 'abcde'
+            click_button 'Create Product'
+
+            expect(page).to have_content 'Price is not a number'
+          end
+        end
+
+        context 'and it is not more than 0' do
+          it 'returns an error for Price' do
+            fill_in 'Price', with: 0
+            click_button 'Create Product'
           end
         end
       end
