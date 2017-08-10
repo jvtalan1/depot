4dd45cb8fd68c68be0f5f26e988704cb8422279d
JV Talan <jvtalan1@up.edu.ph>
2017-08-10 12:24:43 +0800

    Add cart and line item model and pages

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :product do
     title 'Title'
     description 'Description'
-    image_url 'http://www.whatever.com/shirt.jpg'
+    image_url 'http://whatever.com/shirt.jpg'
     price 29
   end
 end
